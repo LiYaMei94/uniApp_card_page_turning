811905ae6f72407e56e89b23498fe1755add6482
辣辣的草莓酱 <wx_4590a3712cc243bd882b44e6ed00520e@git.code.tencent.com>
2020-03-23 16:40:04 +0800

    初始化项目

---
+++ components/SJ-CardPageTurning/SJ-CardPageTurning.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="container"></div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		};
+	},
+	props: {
+		grid_info_arr: {
+			type: Array,
+			default: function() {
+				return [];
+			}
+		},
+		lottery_draw_param: {
+			type: Object,
+			default: function() {
+				return {
+					startIndex: 0, //开始抽奖位置，从0开始
+					totalCount: 4, //一共要转的圈数
+					winingIndex: 2, //中奖的位置，从0开始
+					speed: 50 //抽奖动画的速度 [数字越大越慢,默认100]
+				};
+			}
+		}
+	},
+	onLoad() {},
+
+	methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+@import './SJ-CardPageTurning.scss';
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<CardPageTurning ></CardPageTurning>
+	</view>
+</template>
+
+<script>
+import CardPageTurning from '../../components/SJ-CardPageTurning/SJ-CardPageTurning.vue';
+export default {
+	components:{
+		CardPageTurning
+	},
+	data() {
+		return {
+			
+		};
+	},
+	onLoad() {
+		
+	},
+	methods: {
+		
+		
+	}
+};
+</script>
+
+<style scoped>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+</style>
